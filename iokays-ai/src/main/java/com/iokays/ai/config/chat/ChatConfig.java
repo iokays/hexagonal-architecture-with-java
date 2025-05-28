5b1867494bb02ccdffc18342f74c4c864900d008
彭元彬 <592721086@qq.com>
2025-05-28 23:57:31 +0800

    修改配置.

--- iokays-ai/src/main/java/com/iokays/ai/config/chat/ChatConfig.java
+++ iokays-ai/src/main/java/com/iokays/ai/config/chat/ChatConfig.java
@@ -1,10 +1,9 @@
 package com.iokays.ai.config.chat;
 
+import jakarta.annotation.Nullable;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.mcp.AsyncMcpToolCallback;
-import org.springframework.ai.mcp.AsyncMcpToolCallbackProvider;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,11 +14,12 @@ class ChatConfig {
     @Bean
     ChatClient chatClient(ChatClient.Builder builder,
                           ChatMemory chatMemory,
-                          ToolCallbackProvider toolCallbackProvider) {
-        return builder
-                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
-                .defaultToolCallbacks(toolCallbackProvider.getToolCallbacks())
-                .build();
+                          @Nullable ToolCallbackProvider toolCallbackProvider) {
+        builder.defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build());
+        if (toolCallbackProvider != null) {
+            builder.defaultToolCallbacks(toolCallbackProvider.getToolCallbacks());
+        }
+        return builder.build();
     }
 
 }
