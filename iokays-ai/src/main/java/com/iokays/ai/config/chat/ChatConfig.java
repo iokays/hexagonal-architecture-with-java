ab6b6dc2e558953f924535816d1a64a005e86745
彭元彬 <592721086@qq.com>
2025-07-02 22:56:30 +0800

    添加对向量数据库的支持.

--- iokays-ai/src/main/java/com/iokays/ai/config/chat/ChatConfig.java
+++ iokays-ai/src/main/java/com/iokays/ai/config/chat/ChatConfig.java
@@ -3,8 +3,10 @@ package com.iokays.ai.config.chat;
 import jakarta.annotation.Nullable;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,8 +16,13 @@ class ChatConfig {
     @Bean
     ChatClient chatClient(ChatClient.Builder builder,
                           ChatMemory chatMemory,
-                          @Nullable ToolCallbackProvider toolCallbackProvider) {
-        builder.defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build());
+                          @Nullable ToolCallbackProvider toolCallbackProvider,
+                          VectorStore vectorStore) {
+        builder.defaultAdvisors(
+                MessageChatMemoryAdvisor.builder(chatMemory).build(),
+                QuestionAnswerAdvisor.builder(vectorStore).build()
+        );
+
         if (toolCallbackProvider != null) {
             builder.defaultToolCallbacks(toolCallbackProvider.getToolCallbacks());
         }
--- iokays-ai/src/test/java/com/iokays/ChatClientTest.java
+++ iokays-ai/src/test/java/com/iokays/ChatClientTest.java
@@ -2,10 +2,16 @@ package com.iokays;
 
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.template.st.StTemplateRenderer;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @SpringBootTest
@@ -16,9 +22,10 @@ class ChatClientTest {
 
     @Test
     void test() {
-        log.info("hello world");
-        final Flux<String> content = chatClient.prompt().user("你能做什么呢?").stream().content();
-        log.info("content: {}", content.blockFirst());
+        final Flux<String> content = chatClient.prompt().user("What is Spring").stream().content();
+        // 收集所有响应块并打印
+        List<String> allChunks = content.collectList().block();
+        log.info("Full content: {}", String.join("", CollectionUtils.emptyIfNull(allChunks)));
     }
 
 }
